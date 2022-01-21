c88a1f63fae0bb6fab2db4cf5d13b82afb1c42b1
eroiyo <eroiyo@outlook.es>
2022-01-20 21:02:46 -0400

    nav background

--- js/script.js
+++ js/script.js
@@ -8,14 +8,17 @@ function clickFunction() {
   const para = document.getElementById('toggle-icon');
   const header = document.getElementById("header")
   const logo = document.getElementById("logo")
+  const menu = document.getElementById("menu")
   para.classList.toggle('rotate-icon');
   if (nav2.style.display !== 'flex') {
     logo.textContent = "            "
     header.classList.add("changed-header")
     nav2.style.display = 'flex';
+    menu.classList.add("padding-top")
     logo.classList.remove('not-hidden');
     logo.classList.add('hidden');
   } else {
+    menu.classList.remove("padding-top")
     header.classList.remove("changed-header")
     logo.textContent = "Arturo Ortega"
     nav2.style.display = 'none';
